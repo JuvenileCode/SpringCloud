cfc180c70f6e8768c72d303775fe429cd4946066
bin_l <dota.liu.bin@gmail.com>
2018-06-04 15:53:45 +0800

    spring cloud lessons-1 commit

---
+++ lessons-1/src/main/java/com/sc/lessons1/Lessons1Application.java
@@ -0,0 +1,12 @@
+package com.sc.lessons1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lessons1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lessons1Application.class, args);
+	}
+}
